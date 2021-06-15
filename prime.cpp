59867c1ad3b75a92a1532129cd0d52196cb6127f
Sayon Som <sayonsom11b@gmail.com>
2021-06-16 02:55:28 +0530

    changes made to prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -2,5 +2,15 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin >>n;
+    for(int i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            break;
+        }
+        return;
+    }
     return 0;
 }
